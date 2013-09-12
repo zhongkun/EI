df0329984f1f62acb9246d4a0cece687d45ac7e7
kun <huangzhongkun@douban.com>
2013-09-12 23:35:18 +0800

    add ei manager

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify, request, render_template, abort
+from flask import Flask, jsonify, request, render_template, abort, redirect
 from flask.ext.login import LoginManager, login_required
 
+from models import init_db, Guest
+
 app = Flask(__name__)
-app.config.from_pyfile('config.py', silent=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:kun77416@127.0.0.1:3306/ei"
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+init_db(app)
 
+app.config.from_pyfile('config.py', silent=True)
 
-@app.route('/invite/<string:name>/', methods=['GET', 'POST'])
-def index(name):
-    if not name:
+@app.route('/invite/<string:domain>/', methods=['GET', 'POST'])
+def index(domain):
+    if not domain:
         abort(400)
 
-    current_url = "http://{host}/invite/{name}/".format(host=request.host, name=name)
-    return render_template('index.html', current_url=current_url, guest=str('李强').decode('utf8'))
+    current_url = "http://{host}/invite/{domain}/".format(host=request.host, domain=domain)
+    guest = Guest.query.filter_by(domain=domain).first()
+    print 'guest',guest
+    return render_template('index.html', current_url=current_url, guest=guest, host=request.host)
 
 
 
+@app.route('/manage/', methods=['GET', 'POST'])
+def manager():
+    guests = Guest.query.all()
+    return render_template('manage.html', guests=guests, host=request.host)
+
+@app.route('/guest/add/', methods=['GET', 'POST'])
+def add():
+    name = request.form.get('name')
+    domain = request.form.get('domain')
+
+    
+    guest = Guest(name, domain, request.remote_addr)
+    guest.commit()
+    return redirect('/manage/')
+    
+    
 uwsgi_app = app.wsgi_app
